95a2dfd6ace797c1308fa68719b96c719ef22f34
rimiruma <rimiruma12@gmail.com>
2023-10-27 16:24:15 +0600

    first commit

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,16 @@
+
+
+// const Services = () => {
+//     return (
+//         <div className="mr-4">
+//             <div className="text-center">
+//             <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
+//             <h2 className="tsxt-5xl">Our Service Area</h2>
+//            <p>the majority have suffered alteration in some form, by injected humour, or randomised <br></br> words which do not look even slightly believable. </p>
+//             </div>
+
+//         </div>
+//     );
+// };
+
+// export default Services;
